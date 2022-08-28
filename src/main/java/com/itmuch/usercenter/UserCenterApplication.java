eb684bc31fd7041a1d382efb30700d5aaec01941
xienima <2055472756@qq.com>
2022-08-28 18:02:14 +0800

    init

---
+++ src/main/java/com/itmuch/usercenter/UserCenterApplication.java
@@ -0,0 +1,15 @@
+package com.itmuch.usercenter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import tk.mybatis.spring.annotation.MapperScan;
+
+@MapperScan("com.itmuch")
+@SpringBootApplication
+public class UserCenterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserCenterApplication.class, args);
+	}
+
+}
